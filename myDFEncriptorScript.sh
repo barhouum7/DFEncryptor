a4a3e2d463a8c865811718b3f807b2e406263b6d
barhouum7 <bb.med2000@gmail.com>
2020-07-08 19:22:04 +0100

    Completed my banner function

---
+++ myDFEncriptorScript.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# DFEncriptor v1.0
+# Coded by: BHMBS IBRAHIM Studxent At TI13 ISET Nabeul
+# Github: https://github.com/barhouum7/DFEncriptor
+
+
+
+banner() {
+
+printf "\n"
+printf "\n"
+printf "\n"
+printf "\e[1;90m     ____   __  __ __   ___  ____  _____    ____  __                  __     _            \e[0m\n"
+printf "\e[1;93m    / __ ) / / / //  |_/  / / __ )/ ___/   /  _/ / /_   _____ ____,_ / /_   (_)________  \e[0m\n"
+printf "\e[1;93m   / __ | / /_/ // / |_/ / / __  |\__ \    / /  / __ \ / ___// __  // __ \ / // __  __ \ \e[0m\n"
+printf "\e[1;93m  / /_/ // __  // /  /  / / /_/ /___/ /  _/ /  / /_/ // /   / /_/ // / / // // / / / / / \e[0m\n"
+printf "\e[1;90m /_____//_/ /_//_/  /__/ /_____//____/  /___/ /_.___//_/    \__,_//_/ /_//_//_/ /_/ /_/  \e[0m\n"
+printf "\e[1;90m                                                                                              v1.0\e[0m\n"
+printf "\n"
+printf "\e[1;93m              .:.:.\e[0m\e[1;77m The\e[0m\e[1;92m DFEncriptor\e[0m\e[1;77m Script coded by:\e[0m\e[1;92m GitHub@barhouum7 \e[0m\e[1;93m.:.:.\e[0m\n"
+printf "\n"
+printf "      \e[101m\e[1;77m::                                                                          ::\e[0m\n"
+printf "      \e[101m\e[1;77m::  Bash implementation of a script that can encrypt your files/Folders     ::\e[0m\n"
+printf "      \e[101m\e[1;77m::                                                                          ::\e[0m\n"
+printf "\n"
+
+}
