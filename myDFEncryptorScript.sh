7f4002bf45c093a2ce2714e5ec318428c45ca4aa
Ibrahim BHMBS <bb.med2000@gmail.com>
2022-05-15 00:00:06 +0100

    Typo fixed

--- myDFEncryptorScript.sh
+++ myDFEncryptorScript.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# DFEncriptor v1.0
+# DFEncryptor v1.0
 # Coded by: BHMBS IBRAHIM Studxent At TI13 ISET Nabeul
-# Github: https://github.com/barhouum7/DFEncriptor
+# Github: https://github.com/barhouum7/DFEncryptor
 
 
 
@@ -17,7 +17,7 @@ printf "\e[1;93m  / /_/ // __  // /  /  / / /_/ /___/ /  _/ /  / /_/ // /   / /_
 printf "\e[1;90m /_____//_/ /_//_/  /__/ /_____//____/  /___/ /_.___//_/    \__,_//_/ /_//_//_/ /_/ /_/  \e[0m\n"
 printf "\e[1;90m                                                                                              v1.0\e[0m\n"
 printf "\n"
-printf "\e[1;93m              .:.:.\e[0m\e[1;77m The\e[0m\e[1;92m DFEncriptor\e[0m\e[1;77m Script coded by:\e[0m\e[1;92m GitHub@barhouum7 \e[0m\e[1;93m.:.:.\e[0m\n"
+printf "\e[1;93m              .:.:.\e[0m\e[1;77m The\e[0m\e[1;92m DFEncryptor\e[0m\e[1;77m Script coded by:\e[0m\e[1;92m GitHub@barhouum7 \e[0m\e[1;93m.:.:.\e[0m\n"
 printf "\n"
 printf "      \e[101m\e[1;77m::                                                                          ::\e[0m\n"
 printf "      \e[101m\e[1;77m::  Bash implementation of a script that can encrypt your files/Folders     ::\e[0m\n"
@@ -28,7 +28,7 @@ printf "\n"
 
 
 startMyGame() {
-	printf "                   \e[1;90m     Welcome to my simple\e[0m\e[1;92m DFEncriptor\e[0m\e[1;90m script!    \e[0m\n"
+	printf "                   \e[1;90m     Welcome to my simple\e[0m\e[1;92m DFEncryptor\e[0m\e[1;90m script!    \e[0m\n"
 	printf "\n"
 	printf "\e[33m I am ready to\e[1;33m encrypt\e[0m\e[33m a\e[1;33m file/folder\e[0m\e[33m for you.\e[0m\n"
 	printf "\e[33m Currently I have a limitation, Place me to the\e[0m\e[1;33m same folder\e[0m\e[33m, where a file to be encrypted is present \e[0m\n"
@@ -36,7 +36,7 @@ startMyGame() {
 	printf "\e[1;31m If you still haven't yet installed this library “pinentry-gui”, \n Please, Install it now by using yum or apt package.\e[0m\n\e[1;77m You need just to use one of these commands in another terminal:\e[0m\n\e[1;96m yum install pinentry-gui\n apt-get install pinentry-gui \e[0m\n"
 	printf "\e[1;77m Enter the Exact File Name with extension: \e[0m"
 	read file;
-	if [[ $file == *'.'* && -e ~/myBashProjects/myDFEncriptor/"$file" && ! -L ~/myBashProjects/myDFEncriptor/"$file" ]]; then
+	if [[ $file == *'.'* && -e ~/myBashProjects/myDFEncryptor/"$file" && ! -L ~/myBashProjects/myDFEncryptor/"$file" ]]; then
 		gpg -c $file
 		printf "\n\e[1;92m I have encrypted this file successfully...\e[0m\n"
 	else
